bb87e107d45a4cb63e5c02bd0edc6ce4b302ff04
Erfan Alimardani <mo.erfana6505@yahoo.com>
2025-06-11 18:01:34 +0330

    chore: ignore linking on IOS

---
+++ react-native.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  dependency: {
+    platforms: {
+      ios: null, // prevents autolinking on iOS
+    },
+  },
+};
--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,30 @@
 import { NativeModules, Platform } from 'react-native';
-const { SmsWatcherModule } = NativeModules;
+
+const isAndroid = Platform.OS === 'android';
+const SmsWatcherModule = isAndroid ? NativeModules.SmsWatcherModule : null;
 
 export function setWatchedNumbers(numbers: string[]) {
+  if (!isAndroid || !SmsWatcherModule) return;
   SmsWatcherModule.setTargetNumbers(numbers);
 }
 
 export function addWatchedNumber(number: string) {
+  if (!isAndroid || !SmsWatcherModule) return;
   SmsWatcherModule.addTargetNumber(number);
 }
 
 export function removeWatchedNumber(number: string) {
+  if (!isAndroid || !SmsWatcherModule) return;
   SmsWatcherModule.removeTargetNumber(number);
 }
 
 export function clearWatchedNumbers() {
+  if (!isAndroid || !SmsWatcherModule) return;
   SmsWatcherModule.clearTargetNumbers();
 }
 
 export function getWatchedNumbers(): Promise<string[]> {
-  if (Platform.OS !== 'android') {
+  if (!isAndroid || !SmsWatcherModule) {
     return Promise.resolve([]);
   }
   return SmsWatcherModule.getTargetNumbers();
